3f95443f60cc700bb13fc589bd54dce487e8b2df
John Balke <john.balke@gmail.com>
2021-06-18 12:07:34 +0100

    Add useTable

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -1,88 +1,78 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
+import { Column, useTable } from 'react-table';
 import { Product } from '../data/products';
-
-type SortableFields = 'name' | 'price' | 'stock';
-type SortDirections = 'asc' | 'desc';
 
 type ProductTableProps = {
   products: Product[];
 };
 
 function ProductTable({ products }: ProductTableProps) {
-  const [sortConfig, setSortConfig] =
-    useState<{ field: SortableFields; direction: SortDirections } | null>(null);
+  const data = useMemo(() => [...products], [products]);
 
-  let sortedProducts = useMemo(() => {
-    const sortedProducts = [...products];
+  const columns: Column<Product>[] = useMemo(
+    () => [
+      { Header: 'ID', accessor: 'id' },
+      { Header: 'Name', accessor: 'name' },
+      { Header: 'Price', accessor: 'price' },
+      { Header: 'Stock', accessor: 'stock' },
+    ],
+    []
+  );
 
-    if (sortConfig == null) return sortedProducts;
-
-    sortedProducts.sort((a, b) => {
-      if (a[sortConfig.field] > b[sortConfig.field]) {
-        return sortConfig.direction === 'asc' ? 1 : -1;
-      }
-      if (a[sortConfig.field] < b[sortConfig.field]) {
-        return sortConfig.direction === 'asc' ? -1 : 1;
-      }
-      return 0;
-    });
-
-    return sortedProducts;
-  }, [sortConfig, products]);
-
-  const requestSort = (field: SortableFields): void => {
-    let direction: SortDirections = 'asc';
-    if (sortConfig?.field === field && sortConfig.direction === 'asc')
-      direction = 'desc';
-
-    setSortConfig({ field, direction });
-  };
-
-  const sortClassName = (field: SortableFields) => {
-    return sortConfig?.field === field && sortConfig?.direction === 'asc'
-      ? 'descending'
-      : 'ascending';
-  };
+  const { rows, prepareRow, getTableProps, headerGroups, getTableBodyProps } =
+    useTable({ columns, data });
 
   return (
-    <table>
-      <caption>Our Products</caption>
+    <table {...getTableProps()}>
       <thead>
-        <tr>
-          <th>
-            <button
-              onClick={() => requestSort('name')}
-              className={sortClassName('name')}
-            >
-              Name
-            </button>
-          </th>
-          <th>
-            <button
-              onClick={() => requestSort('price')}
-              className={sortClassName('price')}
-            >
-              Price
-            </button>
-          </th>
-          <th>
-            <button
-              onClick={() => requestSort('stock')}
-              className={sortClassName('stock')}
-            >
-              In Stock
-            </button>
-          </th>
-        </tr>
+        {
+          // Loop over the header rows
+          headerGroups.map((headerGroup) => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                // Loop over the headers in each row
+                headerGroup.headers.map((column) => (
+                  // Apply the header cell props
+                  <th {...column.getHeaderProps()}>
+                    {
+                      // Render the header
+                      column.render('Header')
+                    }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
       </thead>
-      <tbody>
-        {sortedProducts.map((p) => (
-          <tr key={p.id}>
-            <td>{p.name}</td>
-            <td>{p.price}</td>
-            <td>{p.stock}</td>
-          </tr>
-        ))}
+      <tbody {...getTableBodyProps()}>
+        {
+          // Loop over the table rows
+          rows.map((row) => {
+            // Prepare the row for display
+            prepareRow(row);
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {
+                  // Loop over the rows cells
+                  row.cells.map((cell) => {
+                    // Apply the cell props
+                    return (
+                      <td {...cell.getCellProps()}>
+                        {
+                          // Render the cell contents
+                          cell.render('Cell')
+                        }
+                      </td>
+                    );
+                  })
+                }
+              </tr>
+            );
+          })
+        }
       </tbody>
     </table>
   );
--- src/data/products.ts
+++ src/data/products.ts
@@ -16,4 +16,9 @@ export const products: Product[] = [
   { id: 5, name: 'Butter', price: 0.9, stock: 99 },
   { id: 6, name: 'Sour Cream ', price: 2.9, stock: 86 },
   { id: 7, name: 'Fancy French Cheese 🇫🇷', price: 99, stock: 12 },
+  { id: 8, name: 'Oat Milk', price: 1.0, stock: 20 },
+  { id: 9, name: 'Soy Milk', price: 1.2, stock: 32 },
+  { id: 10, name: 'Vegan Cheese', price: 5.5, stock: 12 },
+  { id: 11, name: 'Oat Cream', price: 2.9, stock: 9 },
+  { id: 12, name: 'Double Creams', price: 1.9, stock: 5 },
 ];
